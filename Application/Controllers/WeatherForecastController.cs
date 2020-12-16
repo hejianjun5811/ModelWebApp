9049162a1ede850ed48c86a912a57f798b0c6644
hejj <hejj@atlbattery.com>
2020-12-16 08:58:18 +0800

    添加swagger

--- Application/Controllers/WeatherForecastController.cs
+++ Application/Controllers/WeatherForecastController.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyWebApp.Model;
 
 namespace Application.Controllers
 {
+    /// <summary>
+    /// 天气控制器
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -18,11 +22,19 @@ namespace Application.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+       /// <summary>
+       /// 构造函数
+       /// </summary>
+       /// <param name="logger"></param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// 获取天气信息
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -34,6 +46,15 @@ namespace Application.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        /// <summary>
+        /// 不想显示某些接口,[ApiExplorerSettings(IgnoreApi = true)]或者直接对这个方法 private，也可以直接使用obsolete属性
+        /// </summary>
+        /// <param name="love">model实体类参数</param> 
+        [HttpPost]
+        public void Post(Love love)
+        {
         }
     }
 }
